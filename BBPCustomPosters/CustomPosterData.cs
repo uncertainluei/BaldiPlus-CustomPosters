236d716bfc00ef5301e1cbac63da5e0406003c06
LuisRandomness <73312995+LuisRandomness@users.noreply.github.com>
2024-05-10 01:17:11 +0100

    probable 2024.3 pre-release

--- BBPCustomPosters/CustomPosterData.cs
+++ BBPCustomPosters/CustomPosterData.cs
@@ -1,36 +1,147 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
 using System.Linq;
+using System.Net.NetworkInformation;
+using BepInEx;
+using JetBrains.Annotations;
+using MTM101BaldAPI;
+using MTM101BaldAPI.AssetTools;
 using Newtonsoft.Json;
 using TMPro;
 using UnityEngine;
 
 namespace LuisRandomness.BBPCustomPosters
 {
-    public class CustomWeightedPoster : WeightedPosterObject
+    public class WeightedCustomPoster : WeightedPosterObject
     {
-        public CustomWeightedPoster(PosterObject poster, CustomPosterSettings settings, bool userGenerated = false)
+        public WeightedCustomPoster(CustomPosterObject poster, int weight)
         {
-            this.userGenerated = userGenerated;
-
             selection = poster;
-            weight = settings.posterWeight;
-
-            levelWhitelist = settings.levelWhitelist;
-            reverseWhitelist = settings.reverseWhitelist;
+            customPoster = poster;
+            this.weight = weight;
         }
 
-        private readonly string[] levelWhitelist;
-        private readonly bool reverseWhitelist;
-
-        public readonly bool userGenerated;
-
-        public bool IncludeInLevel(string levelName)
+        public WeightedCustomPoster(CustomPosterObject poster) : this(poster,poster.Weight)
         {
-            // Exclude IF weight = 0 as it won't appear anyway
-            // Include IF either the whitelist is empty OR the level obeys the white/blacklist
-            return weight > 0 && (levelWhitelist.Length == 0 || levelWhitelist.Contains(levelName) != reverseWhitelist);
         }
+
+        public CustomPosterObject customPoster;
+
+        public bool IncludeInLevel(string lvl, int id)
+        {
+            // Include IF either the whitelist is empty OR the level obeys the white/blacklist
+            return customPoster.IncludeInLevel(lvl, id);
+        }
+
+    }
+
+    public class CustomPosterObject : PosterObject
+    {
+        public static CustomPosterObject CreateInstance(string name, PosterPack pack, Texture2D texture, CustomPosterProperties properties)
+        {
+            int width = texture.width, height = texture.height, length = width / height;
+            CustomPosterTextData[] customTextData = properties.textData.Build();
+
+            if (width % height != 0)
+                throw new InvalidDataException($"{pack.packName}: Poster \"{name}\" is in an invalid aspect ratio! Make sure it is in a X:1 ratio!");
+
+            CustomPosterObject poster = CreateInstance<CustomPosterObject>();
+            poster.name = name;
+            poster.baseTexture = texture;
+            poster.textData = customTextData.Where((CustomPosterTextData x) => x.segmentId == 0).ToArray();
+
+            poster.pack = pack;
+
+            poster.weight = properties.posterWeight;
+            poster.global = properties.global;
+
+            poster.levelWhitelist = properties.levelWhitelist;
+            poster.reverseWhitelist = properties.reverseWhitelist;
+
+            if (properties.targetRooms.Length == 0)
+            {
+                poster.targetRooms = new RoomCategory[0];
+            }
+            else
+            {
+                List<RoomCategory> roomCats = new List<RoomCategory>();
+                RoomCategory cat;
+                foreach (string target in properties.targetRooms)
+                {
+                    try
+                    {
+                        cat = EnumExtensions.GetFromExtendedName<RoomCategory>(target);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+
+                    if (!roomCats.Contains(cat))
+                        roomCats.Add(cat);
+                }
+                poster.targetRooms = roomCats.ToArray();
+            }
+
+            // Multi-poster conversion
+            if (length > 1)
+            {
+                PosterObject[] posters = new PosterObject[length];
+                posters[0] = poster; // Set the first object of the multi-poster array as the newly made poster
+
+                int i = 0;
+                for (int x = 0; x < width; x += height) // Re-use height instead of a new local variable cus 1:1 aspect ratio
+                {
+                    Texture2D split = new Texture2D(height, height, TextureFormat.RGBA32, false)
+                    {
+                        filterMode = FilterMode.Point,
+                        name = $"{name}_{i}"
+                    };
+
+                    Color[] pixels = texture.GetPixels(x, 0, height, height);
+                    split.SetPixels(pixels);
+                    split.Apply();
+
+                    if (i == 0)
+                        poster.baseTexture = split;
+                    else
+                    {
+                        posters[i] = ObjectCreators.CreatePosterObject(split, customTextData.Where((CustomPosterTextData y) => y.segmentId == i).ToArray());
+                        posters[i].name = $"{name}_{i}";
+                    }
+                    i++;
+
+                    Destroy(texture); // Frees up the full texture as it is now unused
+                }
+
+                poster.multiPosterArray = posters;
+            }
+
+            return poster;
+        }
+
+        public bool IncludeInLevel(string lvl, int id)
+        {
+            if (!pack.Enabled) return false;
+            if (levelWhitelist.Length == 0) return true;
+
+            if (lvl == "INF" && levelWhitelist.Contains(lvl + id) != reverseWhitelist) return true; // Infinite Floors support
+            return levelWhitelist.Contains(lvl) != reverseWhitelist;
+        }
+
+        public PosterPack pack;
+
+        private string[] levelWhitelist;
+        private bool reverseWhitelist;
+
+        public RoomCategory[] targetRooms;
+        public bool global;
+
+        private int weight;
+        public int Weight => weight > 0 ? weight : pack.DefaultWeight;
     }
 
     public class CustomPosterTextData : PosterTextData
@@ -40,8 +151,8 @@ namespace LuisRandomness.BBPCustomPosters
         public CustomPosterTextData(PosterTextSettings builder)
         {
             textKey = builder.textKey;
-            position = PosterExtensions.Convert(builder.position);
-            size = PosterExtensions.Convert(builder.size);
+            position = builder.position.ToIntVector2();
+            size = builder.size.ToIntVector2();
             fontSize = builder.fontSize;
             segmentId = builder.segmentId;
 
@@ -75,48 +186,13 @@ namespace LuisRandomness.BBPCustomPosters
         }
     }
 
-    public static class PosterExtensions
-    {
-        public static bool IsBlacklisted(this WeightedPosterObject poster)
-        {
-            // Exclude user generated posters
-            if (poster is CustomWeightedPoster && ((CustomWeightedPoster)poster).userGenerated) return false;
-
-            return CustomPostersPlugin.blacklistedPostersRaw.Contains(poster.selection.name.Trim())
-                != CustomPostersPlugin.config_invertForeignPosterBlacklist.Value;
-        }
-
-        public static CustomPosterTextData[] Build(this PosterTextSettings[] customData)
-        {
-            int c = customData.Length;
-            CustomPosterTextData[] newData = new CustomPosterTextData[c];
-            for (int i = 0; i < c; i++)
-                newData[i] = new CustomPosterTextData(customData[i]);
-
-            return newData;
-        }
-
-        public static PosterTextData[] GetTextsForSegment(this CustomPosterTextData[] customData, int segment = 0)
-        {
-            // If array is empty, just return itself
-            if (customData.Length == 0) return customData;
-
-            List<PosterTextData> results = new List<PosterTextData>();
-            foreach (CustomPosterTextData customText in customData)
-                if (customText.segmentId == segment)
-                    results.Add(customText);
-
-            return results.ToArray();
-        }
-
-        public static IntVector2 Convert(SerializableIntVector2 vec)
-        {
-            return new IntVector2(vec.x, vec.y);
-        }
-    }
-
     public class SerializableIntVector2
     {
+        public IntVector2 ToIntVector2()
+        {
+            return new IntVector2(x, y);
+        }
+
         public int x, y;
     }
 
@@ -139,19 +215,24 @@ namespace LuisRandomness.BBPCustomPosters
         public int segmentId = 0;
     }
 
-    public class CustomPosterSettings
+
+    public class CustomPosterProperties
     {
-        // How common/rare it will be in comparison to other posters
-        public int posterWeight = CustomPostersPlugin.config_defaultWeight.Value;
+        [JsonIgnore] public static CustomPosterProperties defaultProperties = new CustomPosterProperties();
 
-        // If higher than 1, then make a multi poster by splitting the image
-        // into multiple textures.
-        [Range(1, 32)]
+        /* How common/rare it will be in comparison to other posters.
+         * If set to 0, this will be set to the default desired value.
+        */
+        [Range(0,float.MaxValue)]
+        public int posterWeight = 0;
+
+        [Obsolete("Poster length is automatically dictated by aspect ratio!")]
         public uint posterLength = 1;
-
-        // A whitelist indicating which levels will contain the poster.
-        // Levels are declared via string values.
-        // If empty, it will spawn in any level.
+        
+        /* A whitelist indicating which levels will contain the poster.
+         *  Levels are declared via string values.
+         *  If empty, it will spawn in any level.
+        */
         public string[] levelWhitelist = new string[0];
 
         // If true, the level whitelist will become a blacklist, and the
@@ -159,6 +240,20 @@ namespace LuisRandomness.BBPCustomPosters
         public bool reverseWhitelist = false;
 
         // PosterTextData entries go here
-        public PosterTextSettings[] textData;
+        public PosterTextSettings[] textData = new PosterTextSettings[0];
+
+        /* Target room categories that the poster will aim to spawn in.
+         * This reflects the RoomCategory enum, and is compatible with extended categories
+         * from other mods.
+         *
+         * Default entries (as of BB+ v0.5):
+         * Null, Hall, Class, Office, Faculty, Test, FieldTrip, Buffer, Special, Mystery
+        */
+        public string[] targetRooms = new string[0];
+
+        /* Are included in the level's global poster pool. 
+         * If false, this will only apply to any room assets.
+        */
+        public bool global = true;
     }
 }
